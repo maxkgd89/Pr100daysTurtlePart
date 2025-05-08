16bd824014b9c01fb4ced0f2c221a081ee8e2648
maxkgd89 <37918183+maxkgd89@users.noreply.github.com>
2025-05-08 00:49:13 +0000

    16D_pretty tables

---
+++ 16_01_PrettyTables_Intro.py
@@ -0,0 +1,12 @@
+
+from prettytable import PrettyTable
+from turtle import Turtle, Screen
+
+# Create table for turtle stats
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
+
+
