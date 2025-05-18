a38943aeab8f0615e01cf4c24d1d266c03096816
maxkgd89 <37918183+maxkgd89@users.noreply.github.com>
2025-05-18 14:43:10 +0300

    20D_2.Snake body follows head
    3-2-1

--- 20_00_Snake.py
+++ 20_00_Snake.py
@@ -25,10 +25,12 @@ for position in starting_position:
 game_is_on = True
 while game_is_on:
     screen.update() #update the screen to show the segments
-    for seg in segments:
-        seg.forward(20)
-        time.sleep(0.25) #pause for a short time to control the speed of the game
-    segments[0].left(90) #move the first segment left
-
-
+    time.sleep(0.25) #pause for a short time to control the speed of the game
+    #for seg_num in range(start,stop,step):
+    for seg_num in range(len(segments)-1,0,-1):
+        new_x=segments[seg_num-1].xcor()
+        new_y=segments[seg_num-1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20) #move the first segment forward
+    segments[0].left(90) #move the first segment forward
 screen.exitonclick()
