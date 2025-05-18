8c588a142a89cff96d2add7a30774876e8a8bc98
maxkgd89 <37918183+maxkgd89@users.noreply.github.com>
2025-05-18 14:33:34 +0300

    20D_1_Snake(add time update)

--- 20_00_Snake.py
+++ 20_00_Snake.py
@@ -1,10 +1,12 @@
 from turtle import Turtle, Screen
+import time
 
 screen= Screen()
 screen.setup(width=600, height=600)
 #screen segment is 20x20
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0) #turn off the screen updates
 
 starting_position = [(0,0), (-20,0), (-40,0)]
 
@@ -18,10 +20,15 @@ for position in starting_position:
     segment.goto(position)
     segments.append(segment)
 
+
 #move the segments
 game_is_on = True
 while game_is_on:
+    screen.update() #update the screen to show the segments
     for seg in segments:
-        seg.backward(20)
+        seg.forward(20)
+        time.sleep(0.25) #pause for a short time to control the speed of the game
+    segments[0].left(90) #move the first segment left
+
 
 screen.exitonclick()
